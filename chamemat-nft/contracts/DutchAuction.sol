6d99dd2cc73ec469beb65d2c888666ec7d85ee76
dongmuel02 <kdh200022@gmail.com>
2025-12-02 10:30:43 +0900

    Update DutchAuction.sol

--- chamemat-nft/contracts/DutchAuction.sol
+++ chamemat-nft/contracts/DutchAuction.sol
@@ -1,10 +1,9 @@
- // SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
 import "./ChamematItem.sol";
 
 contract DutchAuction {
-    {
     ChamematItem public immutable nft;
     uint256 public immutable nftId;
     uint256 public startingPrice;
@@ -14,33 +13,40 @@ contract DutchAuction {
     address payable public seller;
     bool public ended;
 
-    constructor(address _nft, uint256 _nftId, uint256 _startingPrice, uint256 _duration, uint256 _discountRate) {
+    constructor(
+        address _nft,
+        uint256 _nftId,
+        uint256 _startingPrice,
+        uint256 _durationSeconds,
+        uint256 _discountRate
+    ) {
         nft = ChamematItem(_nft);
         nftId = _nftId;
         startingPrice = _startingPrice;
         discountRate = _discountRate;
         startAt = block.timestamp;
-        expiresAt = block.timestamp + _duration;
+        expiresAt = block.timestamp + _durationSeconds;
         seller = payable(msg.sender);
     }
 
-    function getPrice() public view returns (uint256) {
+    function getCurrentPrice() public view returns (uint256) {
         if (block.timestamp >= expiresAt) return 0;
-        uint256 elapsed = block.timestamp - startAt;
-        uint256 discount = discountRate * elapsed;
+        uint256 timeElapsed = block.timestamp - startAt;
+        uint256 discount = discountRate * timeElapsed;
         return startingPrice > discount ? startingPrice - discount : 0;
     }
 
     function buy() external payable {
         require(!ended, "Auction ended");
-        uint256 price = getPrice();
+        uint256 price = getCurrentPrice();
         require(msg.value >= price, "Not enough ETH");
 
         ended = true;
         nft.transferFrom(seller, msg.sender, nftId);
 
-        if (msg.value > price)
+        if (msg.value > price) {
             payable(msg.sender).transfer(msg.value - price);
+        }
         seller.transfer(price);
     }
 }
